d51ded20d6c1bf430dec6b8390f094d834e946d9
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-08-01 11:19:17 +0700

    Create Bridge_Node.sh

---
+++ Bridge_Node.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+echo "==========================================================================================================================="
+echo -e "\033[0;35m"
+echo "  ██████╗ ██████╗ ███╗   ██╗████████╗██████╗ ██╗██████╗ ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗██████╗  █████╗  ██████╗ ";
+echo " ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║██╔══██╗██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗";
+echo " ██║     ██║   ██║██╔██╗ ██║   ██║   ██████╔╝██║██████╔╝██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║██║  ██║███████║██║   ██║";
+echo " ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██╗██║██╔══██╗██║   ██║   ██║   ██║██║   ██║██║╚██╗██║██║  ██║██╔══██║██║   ██║";
+echo " ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║██████╔╝╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝";
+echo "  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ";
+																													  
+echo -e "\033[0;35m"
+echo "==========================================================================================================================="                                                                                    
+sleep 1
+
+source $HOME/.bash_profile
+
+#function Run Celestia Bridge Node
+echo -e "\e[1m\e[32mSetting Up A Celestia Bridge Node \e[0m" && sleep 1
+cd $HOME
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.3.0-rc2
+make install
+
+sleep 3
+celestia bridge init --core.remote tcp://localhost:26657 --core.grpc tcp://localhost:9090
+sleep 3
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-bridge.service
+[Unit]
+Description=celestia-bridge Cosmos daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/go/bin/celestia bridge start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sleep 3
+
+sudo systemctl daemon-reload
+sudo systemctl enable celestia-bridge
+sudo systemctl restart celestia-bridge
+
+echo "==========================================================================================================================="     
+
+
+echo -e '\e[32mCheck your celestia-bridge node status\e[39m' && sleep 3
+if [[ `service celestia-bridge status | grep active` =~ "running" ]]; then
+  echo -e "Your celestia-bridge node \e[32minstalled and running normally\e[39m!"
+else
+  echo -e "Your celestia-bridge node\e[31mwas failed installed\e[39m, Please Re-install."
+fi
