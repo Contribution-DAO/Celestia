2dfb2e242685305d75cacb8d6dbb9355601af28c
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2023-04-03 22:11:20 +0700

    Update Autolight.sh

--- Thai/Light nodes/Autolight.sh
+++ Thai/Light nodes/Autolight.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 function InstallingRequiredtool {
-echo -e "\e[1m\e[32mInstalling required tool ... \e[0m" && sleep 1
-sudo apt install curl -y > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1 && apt install git sudo unzip wget -y > /dev/null 2>&1
-sudo apt install curl tar wget vim clang pkg-config libssl-dev jq build-essential git make ncdu -y > /dev/null 2>&1
+        echo -e "\e[1m\e[32mInstalling required tool ... \e[0m" && sleep 1
+        sudo apt install curl -y > /dev/null 2>&1
+        sudo apt update > /dev/null 2>&1 && apt install git sudo unzip wget -y > /dev/null 2>&1
+        sudo apt install curl tar wget vim clang pkg-config libssl-dev jq build-essential git make ncdu -y > /dev/null 2>&1
 }
 
 
 
 function InstallingGo {
-echo " "
-echo -e "\e[1m\e[32mInstalling Go ... \e[0m" && sleep 1
-ver="1.20"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+        echo " "
+        echo -e "\e[1m\e[32mInstalling Go ... \e[0m" && sleep 1
+        ver="1.20"
+        cd $HOME
+        wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
+        sudo rm -rf /usr/local/go
+        sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+        rm "go$ver.linux-amd64.tar.gz"
+        echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+        source ~/.bash_profile
 }
 
 
@@ -39,21 +39,21 @@ make cel-key
 }
 
 
-function Init{
-echo " "
-echo -e "\e[1m\e[32mInit Create... \e[0m" && sleep 1
-celestia light init --p2p.network blockspacerace >> /root/.celestia-light-blockspacerace-0/keys/my_celes_key.log
+function Init {
+        echo " "
+        echo -e "\e[1m\e[32mInit Create... \e[0m" && sleep 1
+        celestia light init --p2p.network blockspacerace 
 }
 
 
 
 function CreateService {
-echo " "
-echo -e "\e[1m\e[32mCreate celestia-appd.service ... \e[0m" && sleep 1
-sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-lightd.service
-[Unit]
-Description=celestia-lightd Light Node
-After=network-online.target
+        echo " "
+        echo -e "\e[1m\e[32mCreate celestia-appd.service ... \e[0m" && sleep 1
+        sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-lightd.service
+        [Unit]
+        Description=celestia-lightd Light Node
+        After=network-online.target
 
 [Service]
 User=$USER
@@ -71,78 +71,82 @@ systemctl start celestia-lightd
 }
 
 function Restart {
-echo " "
-echo -e "\e[1m\e[32mRestart you node ... \e[0m" && sleep 1
-sudo systemctl restart celestia-lightd
+        echo " "
+        echo -e "\e[1m\e[32mRestart you node ... \e[0m" && sleep 1
+        sudo systemctl restart celestia-lightd
 }
 
 
-function NodeID{
-echo " "
-echo -e "\e[1m\e[32mRestart you node ... \e[0m" && sleep 1
-AUTH_TOKEN=$(celestia light auth admin --p2p.network blockspacerace)
-curl -X POST \
-     -H "Authorization: Bearer $AUTH_TOKEN" \
-     -H 'Content-Type: application/json' \
-     -d '{"jsonrpc":"2.0","id":0,"method":"p2p.Info","params":[]}' \
-     http://localhost:26658
-}
+function NodeID {
+        echo " "
+        echo -e "\e[1m\e[32mRestart you node ... \e[0m" && sleep 1
+        AUTH_TOKEN=$(celestia light auth admin --p2p.network blockspacerace)
+curl -s -X POST      -H "Authorization: Bearer $AUTH_TOKEN"      -H 'Content-Type: application/json'      -d '{"jsonrpc":"2.0","id":0,"method":"p2p.Info","params":[]}'      http://localhost:26658 | jq .result.ID
+                            }
 
 
 
-function Getwallet {
-echo " "
-echo -e "\e[1m\e[32mList you wallet... \e[0m" && sleep 1
-./cel-key list --node.type light --keyring-backend test --p2p.network blockspacerace
-}
+                            function Getwallet {
+                                    echo " "
+                                    echo -e "\e[1m\e[32mList you wallet... \e[0m" && sleep 1
+                                    ./cel-key list --node.type light --keyring-backend test --p2p.network blockspacerace
+                            }
 
 
 
 
 
-echo -e "\e[1m\e[32mMenu V1.2 \e[0m" && sleep 1
+                            echo -e "\e[1m\e[32mMenu V1.2 \e[0m" && sleep 1
 
-PS3='Please enter your choice (input your option number and press enter): '
-options=("Install" "Get Node ID" "Get Wallet" "Restart" "Uninstall" "Quit")
-
-
-
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Install")
-            echo -e '\e[1m\e[32mYou choose Install...\e[0m' && sleep 1
-InstallingRequiredtool
-InstallingGo
-Installingcelestia-app
-Init
-CreateService
-Restart
-echo -e "\e[1m\e[32mYour Node was Install!\e[0m" && sleep 1
-break
-;;
+                            PS3='Please enter your choice (input your option number and press enter): '
+                            options=("Install" "Get Wallet" "Get Node ID" "Restart" "Uninstall" "Quit")
 
 
-"Get Node ID")
-echo -e '\e[1m\e[32mYou choose Add new Peer...\e[0m' && sleep 1
-NodeID
-echo -e "\e[1m\e[32mYour Node was  Add new Peer complete!\e[0m" && sleep 1
-break
-;;
+
+                            select opt in "${options[@]}"
+                            do
+                                        case $opt in
+                                                        "Install")
+                                                                            echo -e '\e[1m\e[32mYou choose Install...\e[0m' && sleep 1
+                                                                            InstallingRequiredtool
+                                                                            InstallingGo
+                                                                            Installingcelestia-app
+                                                                            Init
+                                                                            CreateService
+                                                                            Restart
+                                                                            echo -e "\e[1m\e[32mYour Node was Install!\e[0m" && sleep 1
+                                                                            #break
+                                                                            ;;
 
 
-"Get Wallet")
-echo -e '\e[1m\e[32mYou choose Add new Peer...\e[0m' && sleep 1
-Getwallet
-echo -e "\e[1m\e[32mYour Node was  Add new Peer complete!\e[0m" && sleep 1
-break
-;;
+                                                                    "Get Node ID")
+                                                                            echo -e '\e[1m\e[32mYou choose Get Node ID...\e[0m' && sleep 1
+                                                                            NodeID
+                                                                          
+                                                                            #break
+                                                                            ;;
 
 
-"Quit")
-break
-;;
+                                                                    "Get Wallet")
+                                                                            echo -e '\e[1m\e[32mYou choose Get Wallet...\e[0m' && sleep 1
+                                                                            Getwallet
+                                                                            
+                                                                            #break
+                                                                            ;;
 
-*) echo -e "\e[91minvalid option $REPLY\e[0m";;
-    esac
-done
+
+                                                                    "Restart")
+                                                                            echo -e '\e[1m\e[32mYou choose Restart...\e[0m' && sleep 1
+                                                                            Restart
+                                                                            echo -e "\e[1m\e[32mYour Node was Restart complete!\e[0m" && sleep 1
+                                                                            #break
+                                                                            ;;
+
+
+                                                                    "Quit")
+                                                                            break
+                                                                            ;;
+
+                                                                    *) echo -e "\e[91minvalid option $REPLY\e[0m";;
+                                                                        esac
+                                                                done
